807a430d5413da01663a0b12129f5fe64bd9bb7e
Manav Gakhar <55736716+manavgakhar@users.noreply.github.com>
2021-07-09 13:04:37 +0530

    second commit

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import streamlit as st
 from sentiment import nlpToolkit
 
-st.title("Sentiment Analysis using DistilBERT")
+st.image("data/logo.png",width=None)
+st.markdown(" ## This toolkit is based on pre-trained transformers so processing might take some time lol.")
+
+
+option = st.selectbox('Which feature would you like to use?',('Sentiment Analysis','Part of Speech Tagging','Named Entity Recognition'))
 
 sent = st.text_input("Enter sentence")
 
-if(sent != ""):
-    toolk = nlpToolkit()
+toolk = nlpToolkit()
 
-    result = toolk.sentiment(sent)
+if(sent != "" and option == "Sentiment Analysis"):
+    try:
+        result = toolk.sentiment(sent)
+        st.write("The sentiment is: " + str(result[0]))
+    except:
+        st.text("Invalid input. Please try again.")
 
-    st.text("The sentiment is: " + str(result[0]))
+if(sent != "" and option == "Named Entity Recognition"):
+    try:
+        result = toolk.ner(sent)
+        st.text("The recognised entities are: " + str(result[1]))
+    except:
+        st.text("Invalid input. Please try again.")
+
+if(sent != "" and option == "Part of Speech Tagging"):
+    try:
+        result = toolk.pos(sent)
+        st.write("The POS tags are: " + str(result[1]))
+    except:
+        st.text("Invalid input. Please try again.")
--- sentiment.py
+++ sentiment.py
@@ -19,10 +19,59 @@ class nlpToolkit:
 
 	def sentiment(self,sentence):
 
-		flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
-		s = flair.data.Sentence(sentence)
-		flair_sentiment.predict(s)
-		total_sentiment = s.labels
+		try:
+			flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
+			s = flair.data.Sentence(sentence)
+			flair_sentiment.predict(s)
+			total_sentiment = s.labels
 
-		return total_sentiment
-	
+			return total_sentiment
+
+		except:
+			print("flair sentiment error")
+			return
+
+
+	def pos(self,sentence):
+		try:
+			# make a sentence
+			sentence = flair.data.Sentence(sentence)
+
+			# load the NER tagger
+			tagger = flair.models.SequenceTagger.load("flair/pos-english-fast")
+
+			tagger.predict(sentence)
+
+			entities = []
+			# iterate over entities
+			for entity in sentence.get_spans('pos'):
+			    entities.append(entity)
+
+			return (sentence,entities)
+
+		except:
+			print("flair POS error")
+			return
+
+
+
+	def ner(self,sentence):
+		try:
+			# make a sentence
+			sentence = flair.data.Sentence(sentence)
+
+			# load the NER tagger
+			tagger = flair.models.SequenceTagger.load('ner')
+
+			tagger.predict(sentence)
+
+			entities = []
+			# iterate over entities
+			for entity in sentence.get_spans('ner'):
+			    entities.append(entity)
+
+			return (sentence,entities)
+
+		except:
+			print("flair NER error")
+			return
